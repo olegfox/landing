b630b7cd34f330cd1b5d1ece3080c12018f6c475
olegfox <info@olegfox.com>
2016-03-04 06:05:10 +0300

    Fix video background module header

--- src/Site/MainBundle/Resources/public/frontend/js/main.js
+++ src/Site/MainBundle/Resources/public/frontend/js/main.js
@@ -1,20 +1,22 @@
 $(function(){
-    if(jQuery('.wrap-header').attr('data-video') != undefined) {
-        if(jQuery('.wrap-header').data('video').length > 0) {
-            jQuery('.wrap-header').vide({
-                mp4: jQuery('.wrap-header').data('video')
-            }, {
-                volume: 1,
-                playbackRate: 1,
-                muted: true,
-                autoplay: true,
-                loop: true,
-                position: '50% 50%', // Similar to the CSS `background-position` property.
-                posterType: 'detect', // Poster image type. "detect" — auto-detection; "none" — no poster; "jpg", "png", "gif",... - extensions.
-                resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing...
-            });
+    jQuery('.wrap-header').each(function(i, e) {
+        if(jQuery(e).attr('data-video') != undefined) {
+            if(jQuery(e).data('video').length > 0) {
+                jQuery(e).vide({
+                    mp4: jQuery(e).data('video')
+                }, {
+                    volume: 1,
+                    playbackRate: 1,
+                    muted: true,
+                    autoplay: true,
+                    loop: true,
+                    position: '50% 50%', // Similar to the CSS `background-position` property.
+                    posterType: 'detect', // Poster image type. "detect" — auto-detection; "none" — no poster; "jpg", "png", "gif",... - extensions.
+                    resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing...
+                });
+            }
         }
-    }
+    });
 
     $(".arrow").each(function(i, el) {
         $(el).click(function(e){
